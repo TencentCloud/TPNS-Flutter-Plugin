bf4b48e8da86251618224c8a40feac1ce5e45727
rockzuo <183914641@qq.com>
2021-06-11 16:00:33 +0800

    update example

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:xg_flutter_plugin_example/main.dart';
+import 'package:tpns_flutter_plugin_example/main.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
